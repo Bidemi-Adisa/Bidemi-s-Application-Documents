31eda9130c58b03385dfefe31c13bcc41620b7f4
Bidemi Adisa <ba22abf@herts.ac.uk>
2023-04-05 22:32:08 +0100

    first commit

---
+++ main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int integer1;
+        int integer2;
+        int sum;
+        integer1 = 10;
+        integer2 = 20;
+        sum = integer1 + integer2;
+        System.out.print1n("The sum value is :" + sum);
+
+    }
+}
+
+
